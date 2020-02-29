f6cf721c45d21ea890561860106f806e1773a1bb
Noor ul haq <noor_ul_haq96@hotmail.com>
2020-02-29 20:31:47 +0500

    test EasyButton with ThemeProvider

---
+++ src/components/EasyButton/__tests__/EasyButtonExample.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import EasyButton from "../EasyButton";
+import EasyButtonExample from "../EasyButtonExample";
+import { ThemeProvider, useTheme } from "../theme";
+
+describe("<EasyButtonExample />", () => {
+  let wrapper;
+  let toggler;
+
+  beforeEach(() => {
+    wrapper = mount(<EasyButtonExample />);
+    toggler = wrapper.find("button").filterWhere(n => {
+      return n.text().includes("Toggle theme:");
+    });
+  });
+
+  test("should toggle theme", () => {
+    expect(toggler).toIncludeText("light");
+    toggler.simulate("click");
+    expect(toggler).toIncludeText("dark");
+  });
+
+  test("<EasyButton /> should change styles according to theme", () => {
+    expect(
+      wrapper
+        .find(EasyButton)
+        .find("button")
+        .prop("style")
+    ).toEqual({
+      backgroundColor: "white",
+      color: "black"
+    });
+
+    toggler.simulate("click");
+
+    expect(
+      wrapper
+        .find(EasyButton)
+        .find("button")
+        .prop("style")
+    ).toEqual({
+      backgroundColor: "black",
+      color: "white"
+    });
+  });
+});
+
+/* 
+ // extracting out child components to variables causes issues with state updates
+ // with context or maybe it happens always
+ * https://enzymejs.github.io/enzyme/docs/guides/migration-from-2-to-3.html#calling-props-after-a-state-change
+ * since i'm using props to check styles it's required to use find() after
+ * updating the component
+ */
