f051b75706f9be372f9661f34d9a9964b5ec6990
Noor ul haq <noor_ul_haq96@hotmail.com>
2020-02-29 20:40:00 +0500

    fix typo in variable name

--- src/components/EasyButton/__mocks__/theme.js
+++ src/components/EasyButton/__mocks__/theme.js
@@ -1,5 +1,5 @@
 const mockSetTheme = jest.fn();
 const useTheme = jest.fn().mockImplementation(() => ["light", mockSetTheme]);
-const THhemeProvider = "";
+const ThemeProvider = "";
 
-export { useTheme, THhemeProvider };
+export { useTheme, ThemeProvider };
